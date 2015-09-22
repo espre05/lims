d9337339baed1be1a3aed5b82c05918c61764bc4
Robin Andeer <robin.andeer@gmail.com>
2015-09-22 11:10:32 +0200

    fix loading external config

--- lims/factory.py
+++ lims/factory.py
@@ -6,9 +6,11 @@ from .extensions import lims
 from .settings import BaseConfig
 
 
-def create_app(config_obj=None):
+def create_app(config=None, config_obj=None):
     app = Flask(__name__)
     app.config.from_object(config_obj or BaseConfig)
+    if config is not None:
+        app.config.from_pyfile(config)
     lims.init_app(app)
     app.register_blueprint(api_bp)
     return app
